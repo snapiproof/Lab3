b811f938daf714f9cd24732fb873692e43132438
snapiproof <75568008+snapiproof@users.noreply.github.com>
2020-12-06 15:40:33 +0300

    Update Person.java

--- lab/src/com.metanit/Person.java
+++ lab/src/com.metanit/Person.java
@@ -74,5 +74,20 @@ public class Person extends TextObject implements  Breathe, Tired, See, Carry, A
         return clothes.toString();
         
     }
-
+	
+    @Override
+    public String toString() {
+        return "Person's name is" + this.getName();
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.person, clothes);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return (name == person.name) && clothes.equals(person.clothes);
+    }
 }
