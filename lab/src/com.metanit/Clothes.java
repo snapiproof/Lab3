f35c8de81f81bdfb6d39a0227aafb802f7fb07e2
snapiproof <75568008+snapiproof@users.noreply.github.com>
2020-12-06 15:50:27 +0300

    Update Clothes.java

--- lab/src/com.metanit/Clothes.java
+++ lab/src/com.metanit/Clothes.java
@@ -20,4 +20,26 @@ public class Clothes extends Item {
 	public String getTypeClothes(){
 		return this.typeCloth;
 	}
+	
+	
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Clothes clothes = (Clothes) o;
+        return typeCloth.equals(clothes.typeCloth) &&
+                (free == clothes.free) &&  (name == clothes.name) &&
+                size.equals(bed.size) && color.equals(clothes.color) &&
+		type.equals(clothes.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, size, color, typeCloth);
+    }
+
+    @Override
+    public String toString() {
+        return "Name of ths clothes: " + this.clothes.getName();
+    }
 }
